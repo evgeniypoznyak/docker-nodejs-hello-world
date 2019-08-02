c7881aca6524d8c1d9e4e5fef500af68ad1c5566
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-08-01 19:06:53 -0500

    .env

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
+const test = process.env.SECRET || 'this value set from code';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello world\n');
+    res.send('Hello world\n' + test);
 });
 
 app.listen(PORT, HOST);
